deca0ce4e4cdd35b94c5186c30f445779149e7e4
jobabyyy <joeannaavila123@gmail.com>
2023-01-10 14:11:32 -0600

    prints string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def uppercase(string):
+    for char in string:
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
+            print(chr(ord(char) - ord('a') + ord('A')), end='')
+        else:
+            print(char, end='')
+    print()
