aaff915abac286be14ff2a42e749a6de71109bf3
jobabyyy <joeannaavila123@gmail.com>
2023-01-10 13:16:21 -0600

    checking if num is positive or negative

---
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from random import randint
+number = randint(-10, 10)
+if number > 0:
+    print("The number", number, "is positive\n")
+elif number == 0:
+    print("The number", number, "is zero\n")
+else:
+    print("The number", number, "is negative\n")
