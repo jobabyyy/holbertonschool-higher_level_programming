f06e5b3655aaa6acdd0e3e5f0bf28f8b7bc0e132
jobabyyy <joeannaavila123@gmail.com>
2023-01-10 22:45:44 -0600

    added singular loop and print function

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-result = ''.join(chr(i) for i in range(ord('a'), ord('z')+1) if chr(i) not in ['q','e'])
-print(result)
+alphabet = ""
+for i in range(ord('a'), ord('z')+1):
+    if chr(i) not in ['q', 'e']:
+        alphabet += chr(i)
+print(alphabet)
 
