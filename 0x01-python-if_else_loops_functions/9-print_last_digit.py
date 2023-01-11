5b49a235d59ede076ccf452d9d82ebd2e16ea85c
jobabyyy <joeannaavila123@gmail.com>
2023-01-11 09:44:31 -0600

    removed extra space

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,5 @@ def print_last_digit(number):
     if number < 0:
         number = -number
     last_digit = number % 10
-    print(last_digit, end ='')
+    print(last_digit, end='')
     return last_digit
