9a2a868ac09cbdb9ba36339717358ff89c7fdc4e
jobabyyy <joeannaavila123@gmail.com>
2023-01-10 23:08:04 -0600

    added new line

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
@@ -12,4 +11,4 @@ if last > 5:
 if last == 0:
     print('and is 0')
 if last < 6 and last != 0:
-    print('and is less than 6 and not 0')
+    print('and is less than 6 and not 0\n')
