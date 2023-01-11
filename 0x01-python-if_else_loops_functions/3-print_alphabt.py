0094ceb25476ecc55bdf4589b4deb39111dbb3ff
jobabyyy <joeannaavila123@gmail.com>
2023-01-10 22:42:49 -0600

    created 'result' in order to store string value

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(''.join(chr(i) for i in range(ord('a'), ord('z')+1) if chr(i) not in ['q','e']), end='')
+result = ''.join(chr(i) for i in range(ord('a'), ord('z')+1) if chr(i) not in ['q','e'])
+print(result)
+
