f7eba7c0c0ec29aa4a739ec258082e10fc9fec44
jobabyyy <joeannaavila123@gmail.com>
2023-01-13 23:38:04 -0600

    added str.format()

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(' '.join(str(element) for element in row))
+        print(' '.join('{:d}'.format(element) for element in row))
