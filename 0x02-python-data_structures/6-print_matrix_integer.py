5746af1d4a5b34d4451e792ebbfddc72500b5ec8
jobabyyy <joeannaavila123@gmail.com>
2023-01-13 23:33:53 -0600

    tried a diff approach

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=' ')
-        print()
+        row_str = " ".join(str(x) for x in row)
+        print(row_str)
