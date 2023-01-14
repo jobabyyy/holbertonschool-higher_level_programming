acbd98572f664e3fc5c3d7d75b37fdacd9529b8a
jobabyyy <joeannaavila123@gmail.com>
2023-01-13 23:35:25 -0600

    added string element func

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        row_str = " ".join(str(x) for x in row)
-        print(row_str)
+        print(' '.join(str(element) for element in row))
