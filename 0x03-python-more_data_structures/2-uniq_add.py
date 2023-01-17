8861b17f529e43b8c653d6de48abed473d49b6be
jobabyyy <joeannaavila123@gmail.com>
2023-01-17 15:12:31 -0600

    func to add all unique ints in a list

---
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+def uniq_add(my_list=[]):
+    # convert list to set to remove duplicate elements
+    my_list = set(my_list)
+    # filter out any non-integer element
+    my_list = filter(lambda x: isinstance(x, int), my_list)
+    # return the sum
+    return sum(my_list)
