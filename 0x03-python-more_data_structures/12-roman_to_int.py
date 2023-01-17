34a849ab00d16aea7025f856841777a5211846ed
jobabyyy <joeannaavila123@gmail.com>
2023-01-17 16:31:54 -0600

    attended pep8 errors..again

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,11 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ro_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            total += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
+        if i > 0 and ro_d[roman_string[i]] > ro_d[roman_string[i - 1]]:
+            total += ro_d[roman_string[i]] - 2 * ro_d[roman_string[i - 1]]
         else:
-            total += roman_dict[roman_string[i]]
+            total += ro_d[roman_string[i]]
     return total
-
