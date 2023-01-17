aac7af0c9a9f18c17bfe8524c1f0b36c84947da6
jobabyyy <joeannaavila123@gmail.com>
2023-01-17 16:58:17 -0600

    class square being defined by size

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"This is a Square Class"
+
+
+class Square:
+    "Defines a class named Square"
+    def __init__(self, size):
+        "Initializes the size of one side of square"
+        self._Square__size = size
