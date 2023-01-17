a626c8ae6f1f81917f23c0bc258bbfc9d805bd43
jobabyyy <joeannaavila123@gmail.com>
2023-01-17 17:12:01 -0600

    checking class attritbute

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/isr/bin/python3
+"""This is a square Module"""
+
+
+class Square:
+    """defined class named Square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
