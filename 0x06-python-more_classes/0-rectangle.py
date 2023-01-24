647be236d3013cf0e1aef61597ec875306175566
jobabyyy <joeannaavila123@gmail.com>
2023-01-24 12:57:59 -0600

    updated return

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -7,9 +7,10 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -17,11 +18,11 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
-    
+        self.__width = value
+
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -29,21 +30,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
-
-    def area(self):
-        return self._width * self._height
-
-    def perimeter(self):
-        if self._width == 0 or self._height == 0:
-            return 0
-        return 2 * (self._width + self._height)
-
-    def __str__(self):
-        if self._width == 0 or self._height == 0:
-            return ""
-        else:
-            return '\n'.join(['#'*self._width for i in range(self._height)])
-
-    def __repr__(self):
-        return f"Rectangle({self._width}, {self._height})"
+        self.__height = value
