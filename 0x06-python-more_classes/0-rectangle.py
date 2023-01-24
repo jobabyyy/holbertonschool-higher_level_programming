86bd007e809f38167673a85060d4aa1a14e4eb16
jobabyyy <joeannaavila123@gmail.com>
2023-01-24 12:55:49 -0600

    updated output errors

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -5,9 +5,8 @@
 class Rectangle:
     "Rectangle Class"
     def __init__(self, width=0, height=0):
-        self._width = width
-        self._height = height
-
+        self.width = width
+        self.height = height
     @property
     def width(self):
         return self._width
@@ -19,7 +18,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self._width = value
-
+    
     @property
     def height(self):
         return self._height
@@ -31,3 +30,20 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self._height = value
+
+    def area(self):
+        return self._width * self._height
+
+    def perimeter(self):
+        if self._width == 0 or self._height == 0:
+            return 0
+        return 2 * (self._width + self._height)
+
+    def __str__(self):
+        if self._width == 0 or self._height == 0:
+            return ""
+        else:
+            return '\n'.join(['#'*self._width for i in range(self._height)])
+
+    def __repr__(self):
+        return f"Rectangle({self._width}, {self._height})"
