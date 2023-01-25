c9710da6dded707a5f9ebb73728559ed4774c083
jobabyyy <joeannaavila123@gmail.com>
2023-01-25 11:12:11 -0600

    fix repo

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""doc check"""
+
+def add_integer(a, b=98):
+    """test"""
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
