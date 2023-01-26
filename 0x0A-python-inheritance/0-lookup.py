02c2771aad9d35b3b824b96d8b9cbf1ba30ce227
jobabyyy <joeannaavila123@gmail.com>
2023-01-26 13:25:29 -0600

    added class?

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,3 +2,13 @@
 def lookup(obj):
     return dir(obj)
 
+class MyClass:
+    def __init__(self):
+        self.x = 5
+        self.y = 10
+
+    def my_method(self):
+        return self.x + self.y
+
+my_obj = MyClass()
+print(lookup(my_obj))
