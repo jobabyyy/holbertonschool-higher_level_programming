6a1f9e5190eb3633b81deb6232880fc5687f9579
jobabyyy <joeannaavila123@gmail.com>
2023-01-31 16:25:36 -0600

    raises an expection

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Base Geo Class"""
+
+
+class BaseGeometry:
+    """Raise an Exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
