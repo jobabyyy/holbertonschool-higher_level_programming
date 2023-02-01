88b59a98e3ae9b3b6d8e3b5de3380af4b7f4bad5
jobabyyy <joeannaavila123@gmail.com>
2023-01-31 21:11:33 -0600

    not sure bout this

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""pub method"""
+
+class Student:
+    """load from json file"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        obj_dict = {
+            attr: getattr(self, attr)
+            for attr in dir(self) if not callable(getattr(self, attr))
+            and not attr.startswith("__") and (attrs is None or attr in attrs)
+        }
+        return obj_dict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
