9085bf680fa8f76e22b39e1a4f128c1a8f05d2c8
jobabyyy <joeannaavila123@gmail.com>
2023-01-31 21:18:24 -0600

    diff approach

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""JSON"""
-
+"""import from json"""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-if __name__ == "__main__":
-    """loading..."""
-    try:
-        load_this = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        load_this = []
-    load_this.extend(sys.argv[1:])
-    save_to_json_file(load_this, "add_item.json")
+try:
+    file = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    file = []
+
+argc = len(sys.argv)
+for i in range(1, argc):
+    file.append(sys.argv[i])
+save_to_json_file(file, "add_item.json")
