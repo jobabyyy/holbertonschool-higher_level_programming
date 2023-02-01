a4e53de03be9462e3a086bdb1ec08cf5c52d7f45
jobabyyy <joeannaavila123@gmail.com>
2023-01-31 21:36:10 -0600

    added readme

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """import from json"""
 import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-try:
-    file = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    file = []
-
-argc = len(sys.argv)
-for i in range(1, argc):
-    file.append(sys.argv[i])
-save_to_json_file(file, "add_item.json")
+if __name__ == "__main__":
+    """comment"""
+    try:
+        load_this = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        load_this = []
+    load_this.extend(sys.argv[1:])
+    save_to_json_file(load_this, "add_item.json")
