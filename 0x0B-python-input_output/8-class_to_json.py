74c17747a3f557a8a540002395fd5ace0a291cd0
jobabyyy <joeannaavila123@gmail.com>
2023-01-31 20:44:29 -0600

    function that returns the dictionary

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""CLASS OBJECT"""
+
+def class_to_json(obj):
+    """comment"""
+    obj_dict = {}
+    for attr in dir(obj):
+        if not callable(getattr(obj, attr)) and not attr.startswith("__"):
+            obj_dict[attr] = getattr(obj, attr)
+    return obj_dict
