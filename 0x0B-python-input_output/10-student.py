39f8b59c2a8190117decbfbd431074feffa7f7f3
jobabyyy <joeannaavila123@gmail.com>
2023-01-31 21:02:56 -0600

    class to define student-contin of 9-student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -2,21 +2,18 @@
 """JSON W/FILTER"""
 
 
+
 class Student:
-    """def init"""
+    """class"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        obj_dict = {}
-        if attrs is None:
-            for attr in dir(self):
-                if not callable(getattr(self, attr)) and not attr.startswith("__"):
-                    obj_dict[attr] = getattr(self, attr)
+        if type(attrs) == list:
+            if all(type(attr) == str for attr in attrs):
+                return {ii: getattr(self, ii)
+                        for ii in attrs if hasattr(self, ii)}
         else:
-            for attr in attrs:
-                if hasattr(self, attr):
-                    obj_dict[attr] = getattr(self, attr)
-        return obj_dict
+            return self.__dict__
