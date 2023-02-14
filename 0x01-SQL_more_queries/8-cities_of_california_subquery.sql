3a359ead4e9c5273ff93fd99d0538c6de1e8f5e0
jobabyyy <joeannaavila123@gmail.com>
2023-02-14 14:36:06 -0800

    finally pushing

---
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- CALI GIRLS
+SELECT id, name
+FROM cities
+WHERE state_id IN
+    (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
---
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- CITY ID
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
