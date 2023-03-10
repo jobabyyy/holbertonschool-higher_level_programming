f2554ca28491a27eaa59a4b6b9c678c07f3f32d5
jobabyyy <joeannaavila123@gmail.com>
2023-03-10 15:56:18 -0600

    x

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ with urllib.request.urlopen(url) as response:
     html_content = response.read()
 
 print("Body response:")
-print("\t- type:", type(html_content))
+print("\t- type:", type(html_content)
 print("\t- content:", html_content)
 print("\t- utf8 content:", html_content.decode('utf-8'))
