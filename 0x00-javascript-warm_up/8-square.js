17049038949c7084ee8a820f3f29931dc489e997
jobabyyy <joeannaavila123@gmail.com>
2023-04-09 21:42:58 -0700

    i am a square

---
+++ 0x00-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+
+if (!arg || isNaN(arg)) {
+  console.log("Missing size");
+} else {
+  const size = parseInt(arg);
+
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
+  }
+}
