3efc817ea4dd0981db655c85e1190e9eaf68703c
jobabyyy <joeannaavila123@gmail.com>
2023-04-09 22:02:14 -0700

    updating add function

--- 0x00-javascript-warm_up/9-add.js
+++ 0x00-javascript-warm_up/9-add.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
 function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  }
   return a + b;
 }
 
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+const arg0 = process.argv[2];
+const arg1 = process.argv[3];
 
-if (!arg1 || isNaN(arg1) || !arg2 || isNaN(arg2)) {
+if (!arg0 || isNaN(arg0) || !arg1) {
   console.log('Missing or invalid arguments');
 } else {
-  const num1 = parseInt(arg1);
-  const num2 = parseInt(arg2);
+  const num1 = parseInt(arg0);
+  const num2 = parseInt(arg1);
   const sum = add(num1, num2);
   console.log(sum);
 }
