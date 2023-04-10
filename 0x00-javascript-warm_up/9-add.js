395fb357875e58e0b15ee954aa572e0c80d16e59
jobabyyy <joeannaavila123@gmail.com>
2023-04-09 21:53:21 -0700

    semi --fix

--- 0x00-javascript-warm_up/9-add.js
+++ 0x00-javascript-warm_up/9-add.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-function add(a, b) {
-    return a + b;
-  }
-  
-  const arg1 = process.argv[2];
-  const arg2 = process.argv[3];
-  
-  if (!arg1 || isNaN(arg1) || !arg2 || isNaN(arg2)) {
-    console.log('Missing or invalid arguments');
-  } else {
-    const num1 = parseInt(arg1);
-    const num2 = parseInt(arg2);
-    const sum = add(num1, num2);
-    console.log(sum);
-  }
-  
+function add (a, b) {
+  return a + b;
+}
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (!arg1 || isNaN(arg1) || !arg2 || isNaN(arg2)) {
+  console.log('Missing or invalid arguments');
+} else {
+  const num1 = parseInt(arg1);
+  const num2 = parseInt(arg2);
+  const sum = add(num1, num2);
+  console.log(sum);
+}
