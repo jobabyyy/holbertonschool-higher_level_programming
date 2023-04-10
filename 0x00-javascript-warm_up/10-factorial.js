b1e4bb87dc38b27fe599f36c63837200581c175f
jobabyyy <joeannaavila123@gmail.com>
2023-04-09 22:10:02 -0700

    factorial num

---
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function factorial (n) {
+    if (isNaN(n)) {
+      return 1;
+    }
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  
+  const arg = parseInt(process.argv[2]);
+  
+  console.log(factorial(arg));
+  
