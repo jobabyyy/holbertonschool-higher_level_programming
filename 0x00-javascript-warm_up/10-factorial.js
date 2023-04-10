b33d5cc4fa031d35079836a9c6eef765850780cb
jobabyyy <joeannaavila123@gmail.com>
2023-04-09 22:11:06 -0700

    semistandard fix

--- 0x00-javascript-warm_up/10-factorial.js
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 function factorial (n) {
-    if (isNaN(n)) {
-      return 1;
-    }
-    if (n === 0 || n === 1) {
-      return 1;
-    } else {
-      return n * factorial(n - 1);
-    }
+  if (isNaN(n)) {
+    return 1;
   }
-  
-  const arg = parseInt(process.argv[2]);
-  
-  console.log(factorial(arg));
-  
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const arg = parseInt(process.argv[2]);
+
+console.log(factorial(arg));
