5b879a9e938698ee490dd2ebb7d93f4184217385
jobabyyy <joeannaavila123@gmail.com>
2023-04-09 21:25:21 -0700

    languages are coooool

---
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const LineArgs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < LineArgs.length; i++) {
+  console.log(LineArgs[i]); // loop iterating strings in LineArgs
+}
