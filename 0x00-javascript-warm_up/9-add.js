52573516aceee1f285b605532e7e3440f5290c49
jobabyyy <joeannaavila123@gmail.com>
2023-04-09 21:51:29 -0700

    output errors

--- 0x00-javascript-warm_up/9-add.js
+++ 0x00-javascript-warm_up/9-add.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-
-function add (a, b) {
+function add(a, b) {
     return a + b;
   }
   
-  const arg0 = process.argv[2];
-  const arg1 = process.argv[3];
+  const arg1 = process.argv[2];
+  const arg2 = process.argv[3];
   
-  if (!arg0 || isNaN(arg0) || !arg1 || isNaN(arg1)) {
+  if (!arg1 || isNaN(arg1) || !arg2 || isNaN(arg2)) {
     console.log('Missing or invalid arguments');
   } else {
-    const num1 = parseInt(arg0);
-    const num2 = parseInt(arg1);
+    const num1 = parseInt(arg1);
+    const num2 = parseInt(arg2);
     const sum = add(num1, num2);
     console.log(sum);
   }
