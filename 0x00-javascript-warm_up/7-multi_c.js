c8a34e2fdb7be5c75be6af1a6a41bac7d91032a2
jobabyyy <joeannaavila123@gmail.com>
2023-04-09 21:41:15 -0700

    removed count

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,6 @@ if (!arg || isNaN(arg)) { // checks if num is not null & can be converted
   console.log('Missing number of occurrences');
 } else {
   const x = parseInt(arg); // converting to an integer
-  let count = 0;
 
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
