951702c115938bf88ad5850754496b0b02ba8032
jobabyyy <joeannaavila123@gmail.com>
2023-04-13 13:59:22 -0700

    script that writes a string to file

---
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+const fileContent = process.argv[3];
+
+fs.writeFile(filePath, fileContent, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('The file has been saved!');
+});
