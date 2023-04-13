6461dcb5d7c7d1d5a7ae63a8cd6a0b33b6b17dfd
jobabyyy <joeannaavila123@gmail.com>
2023-04-13 13:52:47 -0700

    print content of a file

---
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(data);
+});
