29bf75a918266fac9c1437b185465aa177bc1e63
jobabyyy <joeannaavila123@gmail.com>
2023-04-14 12:13:55 -0700

    rm

--- 0x01-javascript_objects_scopes_closures/7-main.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const nbOccurrences = require('./7-occurrences').nbOccurences;
-
-console.log(nbOccurrences([1, 2, 3, 4, 5, 6], 3));
-console.log(nbOccurrences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nbOccurrences(["S", 12, "c", "S", "School", 8], "S"));
