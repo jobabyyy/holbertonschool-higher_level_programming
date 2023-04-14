644fa6b302ea8579fb581aae8a5505242c998e7a
jobabyyy <joeannaavila123@gmail.com>
2023-04-14 12:07:29 -0700

    3-starwars_title.js

---
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const movieData = JSON.parse(body);
+  console.log(movieData.title);
+});
