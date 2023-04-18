605d4ab81a75e11ad3691cf3a970be000158310b
jobabyyy <joeannaavila123@gmail.com>
2023-04-18 09:59:04 -0700

    te

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -1,21 +1,24 @@
 #!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file.
+
 const request = require('request');
 const fs = require('fs');
+
+if (process.argv.length < 3) {
+  console.error('Usage: ./5-request_store.js <URL> <output_file_path>');
+  process.exit(1);
+}
+
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.log(error);
-    return;
+    console.error(error);
   }
-
-  fs.writeFile(filePath, body, 'utf8', (err) => {
-    if (err) {
-      console.log(err);
-      return;
+  fs.writeFile(filePath, body, 'utf-8', (error) => {
+    if (error) {
+      console.error(error);
     }
-
-    console.log(`The contents of ${url} have been saved to ${filePath}.`);
   });
 });
