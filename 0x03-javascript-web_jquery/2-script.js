0bd6a3c3489f18170dc1c4d4d40f62a8aba38708
jobabyyy <joeannaavila123@gmail.com>
2023-04-18 15:04:26 -0700

    change color

---
+++ 0x03-javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+// updates the test color when user clicks on tag
+
+$(document).ready(function() {
+    $('#red_header').click(function() {
+      $('header').css('color', '#FF0000');
+    });
+  });
+  
