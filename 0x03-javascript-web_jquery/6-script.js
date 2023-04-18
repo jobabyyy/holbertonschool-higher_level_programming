4b18fad35e11dfefd6414bbc995a5bc0d120a230
jobabyyy <joeannaavila123@gmail.com>
2023-04-18 15:12:15 -0700

    New Header !!

---
+++ 0x03-javascript-web_jquery/6-script.js
@@ -0,0 +1,8 @@
+// script that updates text and addes New Header !!
+
+$(document).ready(function() {
+    $('#update_header').click(function() {
+      $('header').text('New Header!!!');
+    });
+  });
+  
