1a20cd8e6e753b6416700eddcd7960b0993f58f6
jobabyyy <joeannaavila123@gmail.com>
2023-04-18 15:13:56 -0700

    fetching name

---
+++ 0x03-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+// script to fetch char name
+
+$(document).ready(function() {
+    $.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function(data) {
+      $('#character').text(data.name);
+    });
+  });
+  
